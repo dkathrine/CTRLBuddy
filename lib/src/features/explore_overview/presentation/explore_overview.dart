3944b5578460487dfb2ee29ef9e0383c83ac00d5
Kathrine <101878099+dkathrine@users.noreply.github.com>
2025-08-29 14:11:25 +0200

    created and added SearchCard to explore_overview

--- lib/src/features/explore_overview/presentation/explore_overview.dart
+++ lib/src/features/explore_overview/presentation/explore_overview.dart
@@ -1,5 +1,6 @@
 import 'package:ctrl_buddy/src/common/widgets/message_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:ctrl_buddy/src/common/widgets/search_card.dart';
 
 class ExploreOverview extends StatefulWidget {
   const ExploreOverview({super.key});
@@ -15,7 +16,39 @@ class _ExploreOverviewState extends State<ExploreOverview> {
       children: [
         SizedBox(height: 8),
         MessageBar(hintText: "Search"),
-        SingleChildScrollView(child: Wrap(children: [])),
+        SizedBox(height: 16),
+        Expanded(
+          child: SingleChildScrollView(
+            child: Wrap(
+              spacing: 16,
+              runSpacing: 16,
+              children: [
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+                SearchCard(),
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
