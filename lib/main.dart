b00edc78210222e39db63fdc31a6b4e5ed734938
Kathrine <101878099+dkathrine@users.noreply.github.com>
2025-09-15 11:41:19 +0200

    added firebase to ctrlbuddy

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:ctrl_buddy/src/data/mock_db.dart';
 import 'package:flutter/material.dart';
 import 'package:ctrl_buddy/src/app.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     Provider<MockDatabase>(
       create: (_) => MockDatabase(),
